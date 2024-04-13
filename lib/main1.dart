e2bb6a07ece7e5a9fffe2a1cb5f73dfb17ffc6a8
David Osorio <davidosoriosuarez17@gmail.com>
2024-04-13 01:27:08 -0500

    cccccccccccccc

--- lib/main1.dart
+++ lib/main1.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'hola xd: dddd',
+              'hola xd: hhh',
             ),
             Text(
               '$_counter',
